67fbbcdc6ac3963850d94b1ed493a9bde4b33fbc
Pavitra <pavitragolchha@gmail.com>
2018-02-19 08:48:11 +0530

    Fix creation of tables

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 # from datetime import datetime
+import os
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pavitra24@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
 class TinyWebDB(db.Model):
+    __tablename__ = 'tinywebdb'
     tag = db.Column(db.String, primary_key=True, nullable=False)
     value = db.Column(db.String, nullable=False)
     # The 'date' column is needed for deleting older entries, so not really required
     # date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+db.create_all()
+db.session.commit()
 
 
 @app.route('/')
@@ -24,8 +30,8 @@ def hello_world():
 
 @app.route('/storeavalue', methods=['POST'])
 def store_a_value():
-    tag = request.args.get('tag', '')
-    value = request.args.get('value')
+    tag = request.form['tag']
+    value = request.form['value']
     if tag:
         # Prevent Duplicate Key error by updating the existing tag
         existing_tag = TinyWebDB.query.filter_by(tag=tag).first()
@@ -36,15 +42,16 @@ def store_a_value():
             db.session.add(data)
             db.session.commit()
         return jsonify(['STORED', tag, value])
-    return 'Enter a valid tag!'
+    return 'Invalid Tag!'
 
 
 @app.route('/getvalue', methods=['POST'])
 def get_value():
-    tag = request.args.get('tag', '')
+    tag = request.form['tag']
     if tag:
         value = TinyWebDB.query.filter_by(tag=tag).first().value
         return jsonify(['VALUE', tag, value])
+    return 'Invalid Tag!'
 
 
 @app.route('/deleteentry')
@@ -53,5 +60,4 @@ def delete_entry():
 
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
