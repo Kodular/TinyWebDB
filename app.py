832cd3188b1bc8c056c5db21e04c0db9dc904e9c
Pavitra Golchha <hello@pavi2410.me>
2022-08-11 19:57:48 +0000

    Rename env var

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
